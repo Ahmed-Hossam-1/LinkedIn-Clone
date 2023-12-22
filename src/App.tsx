f84c48fffe4310818f2e40413efd35dda1d122f6
Ahmed Hossam <128134339+Ahmed-Hossam-1@users.noreply.github.com>
2023-12-22 19:38:09 +0200

    add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./layout/Layout";
+import Login from "./auth/Login";
+
 const App = () => {
-  return <div>App</div>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route path="login" element={<Login />} />
+      </Route>
+    )
+  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
